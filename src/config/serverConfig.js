86fdba640ac26050623b08c5d3cf2dcdb072c163
Luong Hung <cn.ben1059@gmail.com>
2019-07-18 22:34:54 +0700

    update

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-    host: 'https://ezchat-realtime.herokuapp.com',
+    host: '0.0.0.0',
     port: 4000,
     mongo: "mongodb+srv://hungvinh:CeeW6sBrFDUlgT5Q@chatrealtime-jdhrb.mongodb.net/chatrealtime?retryWrites=true&w=majority"
 }
